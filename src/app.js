ee1c61144c06477beee0568c2e16febc4f84c0ae
Raii <143903432+raiinamoor@users.noreply.github.com>
2025-02-21 15:40:46 +0300

    Обозначить номера заданий в исполняемом коде

--- src/app.js
+++ src/app.js
@@ -5,18 +5,24 @@ const words = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
 
 const isShorterThan5 = item => item.length < 5
 
-
+// задание 1
+console.log("Задание 1")
 console.log(`Результат функции getEvenNumbers для ${numbers}: ${getEvenNumbers(numbers)}`)
 console.log(`Результат функции squareNumbers для ${numbers}: ${squareNumbers(numbers)}`)
 console.log(`Результат функции getItemsWithProperty для ${words}: ${getItemsWithProperty(words, isShorterThan5)}`)
 console.log(`Результат функции addNumbers для ${numbers}: ${addNumbers(numbers)}`)
+console.log()
 
+// задание 2
+console.log("Задание 2")
 console.log(
     `Результат функции applyToAll для ${words}: ` +
     `${applyToAll(words, (word) => word.toUpperCase())}`
 )
+console.log()
 
-
+// задание 3
+console.log("Задание 3")
 let evenNumbers = getEvenNumbers(numbers)
 let squaredEvenNumbers = squareNumbers(evenNumbers)
 let sumOfSquares = addNumbers(squaredEvenNumbers)
